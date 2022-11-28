8f556967b4f255fc50b420696ea05c15a5f05460
RohanG9929 <103215628+RohanG9929@users.noreply.github.com>
2022-11-28 13:45:07 -0500

    Add metrics and plotting files

--- TRAINING_withVal.py
+++ TRAINING_withVal.py
@@ -1,3 +1,10 @@
+# 1- define optimizer
+# 2- one step through optimizer
+# 3- train val inference
+# 4-  training_step
+# 5- training epoch end
+# 6- compute metrics
+
 import os
 import sys
 import tensorflow as tf
@@ -12,20 +19,50 @@ from src.loftr.LoFTR_TF import LoFTR
 from src.training.supervisionTF import compute_supervision_coarse, compute_supervision_fine
 from src.training.loftr_lossTF import LoFTRLoss
 from src.training.datasets.LoadDataMD import read_fullMD_data
-from src.loftr.utils.plotting_TF import make_matching_figure
+from src.loftr.utils.plotting_TF import make_matching_figure, make_matching_figures
 from src.configs.getConfig import giveConfig
+from src.training.metricsTF import (
+    compute_symmetrical_epipolar_errors,
+    compute_pose_errors,
+    aggregate_metrics
+)
+
 tf.config.run_functions_eagerly(True)
 
 config,_config = giveConfig()
 checkpointPath = "./weights/fullMD/cp_smallMegadepth.ckpt"
 
 optimizer_1=tf.keras.optimizers.Adam(learning_rate=0.001)
+# optimizer_2 = tf.keras.optimizers.experimental.AdamW()
+# train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+# val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+
 matcher=LoFTR(config=_config['loftr']) 
 modelLoss=LoFTRLoss(_config) 
 
+
+
 ##############################
-#Init Training
+#Init Training Functinos
 ##############################
+
+def _compute_metrics(self, batch):
+
+    compute_symmetrical_epipolar_errors(batch)  # compute epi_errs for each match
+    compute_pose_errors(batch, config)  # compute R_errs, t_errs, pose_errs for each pair
+
+    rel_pair_names = list(zip(*batch['pair_names']))
+    bs = batch['image0'].shape[0]
+    metrics = {
+        # to filter duplicate pairs caused by DistributedSampler
+        'identifiers': ['#'.join(rel_pair_names[b]) for b in range(bs)],
+        'epi_errs': [batch['epi_errs'][batch['m_bids'] == b].cpu().numpy() for b in range(bs)],
+        'R_errs': batch['R_errs'],
+        't_errs': batch['t_errs'],
+        'inliers': batch['inliers']}
+    ret_dict = {'metrics': metrics}
+    return ret_dict, rel_pair_names
+
 @tf.function
 def train_step(data):
     '''
@@ -39,15 +76,35 @@ def train_step(data):
 
     grads = tape.gradient(lossData['loss'], matcher.trainable_weights, unconnected_gradients='zero')
     optimizer_1.apply_gradients(zip(grads, matcher.trainable_weights))
-    # print("Weights Updated")
 
     return lossData['loss']
+
+@tf.function
+def validation_step(data):
+    superVisionData = compute_supervision_coarse(data,config)#Ground Truth generation
+    modelData = matcher(superVisionData, training = True)
+    fineSuperData = compute_supervision_fine(modelData,config)
+    batch = modelLoss(fineSuperData)
+    ret_dict, _ = _compute_metrics(batch)
+    
+    val_plot_interval = 1#max(numValBatches // self.n_vals_plot, 1)
+    figures = {'evaluation': []}
+    # if batch_idx % val_plot_interval == 0:
+    figures = make_matching_figures(batch, config)
+
+    return {
+        **ret_dict,
+        'loss_scalars': batch['loss_scalars'],
+        'figures': figures,
+    }
 
 root_dir = './src/training/datasets/megadepth/'
 epochs = 10
 scenes = read_fullMD_data(batch_size=4,npz_dir= os.path.join(root_dir,'megadepth_indices/scene_info_0.1_0.7/*'),root_dir=root_dir)
+validation_scenes = []#read validation data 
 logger.info(f"Data Loaded!")
 loss_all=[]
+validation_all=[]
 logger.info(f"Trainer initialized!")
 
 ##############################
@@ -55,8 +112,12 @@ logger.info(f"Trainer initialized!")
 ##############################
 for epoch in range(epochs):
     loss=0
+    #Epoch begin
     for batch in tqdm((scenes),desc='Running Epoch '+str(epoch)):
         loss+=train_step(batch)
+    
+    #Epoch end    
+    validation_all.append(validation_step(validation_scenes[epoch]))
     print(f'loss for epoch {epoch} is {tf.math.reduce_sum(loss)/(len(scenes))}')
     loss_all.append(float(tf.math.reduce_sum(loss)/(len(scenes))))
 
